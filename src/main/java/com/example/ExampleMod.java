a6c9198fd8a7d15a7fd970a23e870c978136d912
kelemlucht70-collab <kelemlucht70@gmail.com>
2026-08-19 22:38:42 -0300

    Corrige sintaxe e estrutura do ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,29 +1,15 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-    public static final String MOD_ID = "discordchatmod";
+    public static final String MOD_ID = "modid";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        // Escuta quando um jogador manda mensagem no chat
-        ServerMessageEvents.CHAT.register((message, sender, params) -> {
-            String playerName = sender.getName().getString();
-            String messageContent = message.getContent().getString();
-            DiscordWebhook.sendMessage(playerName, messageContent);
-        });
+        LOGGER.info("Hello Fabric world!");
     }
-}
-
-		LOGGER.info("Hello Fabric world!");
-	}
-
-	public static Identifier id(String path) {
-		return Identifier.fromNamespaceAndPath(MOD_ID, path);
-	}
 }
